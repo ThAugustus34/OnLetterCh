f19404b30d8a4f67389850337142ce03e5ab6c83
LastCeri <149306745+ThAugustus34@users.noreply.github.com>
2023-10-31 19:51:58 +0300

    Firebase Add & Ui fix

---
+++ letter-future/BackEnd/Database/Database.tsx
@@ -0,0 +1,22 @@
+import { doc, setDoc } from "firebase/firestore/lite";
+import { db } from './firebaseconfig';
+
+const addLetterToFirestore = async (content: string, deliveryDate: string, recipientEmail: string) => {
+    try {
+        const newLetter = {
+            content,
+            deliveryDate,
+            recipientEmail,
+            timestamp: new Date().toISOString()
+        };
+
+        const docRef = doc(db, "letters");
+        await setDoc(docRef, newLetter);
+
+        console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+        console.error("Error adding document: ", error);
+    }
+};
+
+export  {addLetterToFirestore};
