1c55b49078a7215d745c496d87c96a1cbeaf3ec5
TUsluRa <149306745+ThAugustus34@users.noreply.github.com>
2023-11-04 22:33:20 +0300

    Add Backend Server Post/Request

--- letter-future/src/FrontEnd/WalletProc/Connection.tsx
+++ letter-future/src/FrontEnd/WalletProc/Connection.tsx
@@ -1,13 +1,14 @@
 
+import {Request} from '../../BackEnd/Api/Api';
+
 declare global {
     interface Window { mina: any; } 
   }
   
   export type SendPaymentParams = {
-    amount: string;
-    to: string;
-    fee: string;
-    memo: string;
+    content:string,
+    email:string,
+    deliverydate:string 
   };
 
   export async function checkMinaProvider(): Promise<boolean> {
@@ -19,26 +20,44 @@ declare global {
   }
   
   export async function sendPayment({
-    amount,
-    to,
-    fee,
-    memo
+    content, 
+    email,
+    deliverydate, 
   }: SendPaymentParams): Promise<string> {
     if (!(await checkMinaProvider())) {
       throw new Error('No provider was found. Please install Auro Wallet.');
     }
   
     try {
+      const requestData = await Request({ GetTransactionData: "0" });
       const sendResult = await window.mina.sendLegacyPayment({
-        amount, to, fee, memo
+        amount: requestData.lettersendamount, 
+        to: requestData.MainWalletAdress,
+        fee: requestData.fee,
+        memo: requestData.memo
       });
-  
       if (sendResult && 'hash' in sendResult) {
-        return sendResult.hash;
+        let accounts;
+        try {
+          accounts = await window.mina.getAccounts();
+          if (!accounts || accounts.length === 0) {
+            throw new Error('No accounts found.');
+          }
+        } catch (accountsError) {
+          console.error('Failed to retrieve accounts:', accountsError);
+          throw new Error('Payment was sent, but account retrieval failed.');
+        }
+        try {
+          return sendResult.hash;
+        } catch (firestoreError) {
+          console.error('Failed to update Firestore:', firestoreError);
+          throw new Error('Payment was sent, accounts retrieved, but Firestore update failed.');
+        }
       } else {
         throw new Error(sendResult?.message || 'Payment failed with no transaction hash returned.');
       }
     } catch (error) {
+      console.error('Payment failed:', error);
       throw error;
     }
   }
